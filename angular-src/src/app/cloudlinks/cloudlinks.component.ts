d5bfcf42418536bf4be44d67f4358b8d5b8cb05e
Misha <mishaOvodenko@gmail.com>
2018-02-01 21:12:16 +0200

    creating table myself

--- angular-src/src/app/cloudlinks/cloudlinks.component.ts
+++ angular-src/src/app/cloudlinks/cloudlinks.component.ts
@@ -8,75 +8,76 @@ import { Ng2SmartTableModule } from 'ng2-smart-table';
   styleUrls: ['./cloudlinks.component.css']
 })
 export class CloudlinksComponent implements OnInit {
-  settings:any;
-  data:any;
+  count:any = 0;
+  tables:any = {};
+  add_row_buttons:any = {};
   constructor() { 
-    
   }
 
   ngOnInit() {
-    this.settings = {
-      attr:{
-        class:"input"
-      },
-      actions:{
-        position:'right'
-      },
-      columns: {
-        num: {
-          title: 'Number',
-          editable:false,
-          class:"misha"
-        },
-        name: {
-          title: 'Name',
-          class:"misha"
-        },
-        desc: {
-          title: 'Description',
-          class:"misha"
-        },
-        link: {
-          title: 'Link',
-          type:'html',
-          class:"misha"
-        }
-      }
-    };
-    this.data=[
-      {
-        num: 1,
-        name: 'Leanne Graham',
-        desc: 'Bret',
-        link: '<a href="http://www.google.com">Google</a>',
-      },
-      {
-        num: 2,
-        name: 'Ervin Howell',
-        desc: 'Antonette',
-        link: '<a href="https://github.com/akveo/ng2-admin">Ng2 Admin</a>',
-      },
-      {
-        num: 3,
-        name: 'Clementine Bauch',
-        desc: 'Samantha',
-        link: '<a href="https://github.com/akveo/ng2-smart-table">Ng2 smart table</a>',
-      },
-      {
-        num: 4,
-        name: 'Patricia Lebsack',
-        desc: 'Karianne',
-        link: '<a href="https://github.com/akveo/blur-admin">Blur Admin</a>',
-      },
-    ];
-    
+   }
 
-    
+  addTable(){
+    var tbl_cont = document.getElementById('tbls');
+    var tbl_header = document.createElement('h3');
+    tbl_header.innerHTML="Table 1";
+    tbl_header.className="text-center"
+    var tbl = document.createElement('table');
+    tbl.className="table table-bordered table-dark"
+    tbl.setAttribute("id","table1");
+    var tbl_body = document.createElement('tbody');
+    var header_row = document.createElement('tr');
+    var id_header = document.createElement('th');
+    id_header.innerText="Number"
+    var name_header = document.createElement('th');
+    name_header.innerHTML="Name"
+    var desc_header = document.createElement('th');
+    desc_header.innerHTML="Description"
+    var link_header = document.createElement('th');
+    link_header.innerHTML="Link"
+    var add_row_button =document.createElement('button')
+    add_row_button.className="btn btn-primary"
+    add_row_button.innerHTML="add row"
+    add_row_button.onclick=this.addRow;
+    tbl_cont.appendChild(tbl_header);
+    header_row.appendChild(id_header);
+    header_row.appendChild(name_header);
+    header_row.appendChild(desc_header);
+    header_row.appendChild(link_header);
+    header_row.appendChild(add_row_button)
+    tbl_body.appendChild(header_row);
+    tbl.appendChild(tbl_body);
+    tbl_cont.appendChild(tbl);
+  }
+  addRow(){
+    this.count=this.count+1;
+    var table = document.getElementById("table1");
+    var header_row = document.createElement('tr');
+    var id_header = document.createElement('td');
+    id_header.innerHTML=this.count.toString();
+    var name_header = document.createElement('td');
+    name_header.setAttribute("placeholder","Name")
+    name_header.contentEditable="true"
+    var desc_header = document.createElement('td');
+    desc_header.setAttribute("placeholder","Description")
+    desc_header.contentEditable="true"
+    var link_header = document.createElement('td');
+    link_header.setAttribute("placeholder","Link")
+    link_header.contentEditable="true"
+    var edit_row_button =document.createElement('button')
+    edit_row_button.className="btn btn-danger"
+    edit_row_button.innerHTML="edit"
+    edit_row_button.onclick=this.editRow;
+    header_row.appendChild(id_header);
+    header_row.appendChild(name_header);
+    header_row.appendChild(desc_header);
+    header_row.appendChild(link_header);
+    header_row.appendChild(edit_row_button); 
+    table.appendChild(header_row);
   }
 
-  // addTable(){
-  //   var tablechild = document.createElement('table');
+  editRow(){
 
-  // }
+  }
 
 }
