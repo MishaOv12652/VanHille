578a3b1d991daed348bfdc7e52328ab420fb2712
Misha <mishaOvodenko@gmail.com>
2018-02-13 20:58:36 +0200

    added router export and modal add row form

--- routes/cloudlinks.js
+++ routes/cloudlinks.js
@@ -2,46 +2,48 @@ var express = require('express');
 var router = express.Router();
 const CloudLink = require('../models/cloudlinks');
 
-router.get('/getAllCloudLinks',(req,res,next)=>{
-    CloudLink.getAllCloudLinkTables((err,cloudLinksTables)=>{
-        if(err){
-            res.json({success:false,msg:"אין טבלאות של קישורים לענן"})
-        }else{
-            res.json({success:true,cloudLinksTables:cloudLinksTables});
+router.get('/getAllCloudLinks', (req, res, next) => {
+    CloudLink.getAllCloudLinkTables((err, cloudLinksTables) => {
+        if (err) {
+            res.json({ success: false, msg: "אין טבלאות של קישורים לענן" })
+        } else {
+            res.json({ success: true, cloudLinksTables: cloudLinksTables });
         }
     });
 });
 
-router.get('/:CloudLinkTableId',(req,res,next)=>{
-    Question.getQuestion(req.params.CloudLinkTableId,(err,cloudLinksTable)=>{
-        if(err){
-            res.json({success:false,msg:"לא נמצאה הטבלה"});
-        }else{  
-            res.json({success:true,cloudLinksTable:cloudLinksTable});
+router.get('/:CloudLinkTableId', (req, res, next) => {
+    Question.getQuestion(req.params.CloudLinkTableId, (err, cloudLinksTable) => {
+        if (err) {
+            res.json({ success: false, msg: "לא נמצאה הטבלה" });
+        } else {
+            res.json({ success: true, cloudLinksTable: cloudLinksTable });
         }
     });
 });
 
-router.post('/addCloudLinkTable',(req,res,next)=>{
+router.post('/addCloudLinkTable', (req, res, next) => {
     let newCloudLinkTable = new CloudLink({
-        tableId:req.body.tableId,
-        Table:req.body.Table
+        tableId: req.body.tableId,
+        Table: req.body.Table
     });
-    CloudLink.addCloudLinkTable(newCloudLinkTable,(err,cloudLinkTable)=>{
-        if(err){
-            res.json({success:false,msg:"קרתה שגיאה בהוספה טבלה של קישור לענן"});
-        }else{
-            res.json({success:true,cloudLinkTable:cloudLinkTable});
+    CloudLink.addCloudLinkTable(newCloudLinkTable, (err, cloudLinkTable) => {
+        if (err) {
+            res.json({ success: false, msg: "קרתה שגיאה בהוספה טבלה של קישור לענן" });
+        } else {
+            res.json({ success: true, cloudLinkTable: cloudLinkTable });
         }
     });
 });
 
-router.post('/:CloudLinkTableId',(req,res,next)=>{
-    updateCloudLinkTable.updateCloudLinkTable(req.params.CloudLinkTableId,req.body.cloudLinksTable,(err,updatedCloudLinksTable)=>{
-        if(err){
-            res.json({success:false,msg:"תקלה בעדכון טבלה של קישור לענן"});
-        }else{
-            res.json({success:true,updatedCloudLinksTable:updatedCloudLinksTable});
+router.post('/:CloudLinkTableId', (req, res, next) => {
+    updateCloudLinkTable.updateCloudLinkTable(req.params.CloudLinkTableId, req.body.cloudLinksTable, (err, updatedCloudLinksTable) => {
+        if (err) {
+            res.json({ success: false, msg: "תקלה בעדכון טבלה של קישור לענן" });
+        } else {
+            res.json({ success: true, updatedCloudLinksTable: updatedCloudLinksTable });
         }
     })
 });
+
+module.exports = router;
