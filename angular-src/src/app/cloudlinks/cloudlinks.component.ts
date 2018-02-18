44ee12d3c86f5615ad72632d2f582cd8f3c43050
Misha <mishaOvodenko@gmail.com>
2018-02-18 16:02:32 +0200

    cloud links update entry working

--- angular-src/src/app/cloudlinks/cloudlinks.component.ts
+++ angular-src/src/app/cloudlinks/cloudlinks.component.ts
@@ -16,14 +16,14 @@ export class CloudlinksComponent implements OnInit {
   tables_array: Array<Object>;
 
 
-  constructor(private  cloud_links_service: CloudlinksService,private flashmessage: FlashMessagesService) {
+  constructor(private  cloud_links_service: CloudlinksService, private flashmessage: FlashMessagesService) {
   }
 
   ngOnInit() {
     this.cloud_links_service.getAllCloudLinkTables().subscribe(tables => {
-      if(!tables.success){
-        this.flashmessage.show(tables.msg,{ cssClass: 'alert-danger', timeout: 3000 })
-      }else {
+      if (!tables.success) {
+        this.flashmessage.show(tables.msg, {cssClass: 'alert-danger', timeout: 3000})
+      } else {
         this.tables_array = tables.cloudLinksTables;
       }
 
@@ -41,19 +41,19 @@ export class CloudlinksComponent implements OnInit {
       edit: {
         confirmSave: true,
       },
-      attr:{
+      attr: {
         id: this.tableName,
-        class:'table misha'
+        class: 'table misha'
       },
-      pager:{
-        display:true,
-        perPage:10
+      pager: {
+        display: true,
+        perPage: 10
       },
       columns: {
         id: {
           title: 'ID',
-          filter:{
-            sortDirection:'asc'
+          filter: {
+            sortDirection: 'asc'
           }
         },
         name: {
@@ -64,32 +64,42 @@ export class CloudlinksComponent implements OnInit {
         },
         url: {
           title: 'URL',
-          type:'html'
+          type: 'html'
         }
       }
     };
     this.tables_array.push({tableId: this.tableName, settings_obj: this.table_settings_object, data: []});
-    this.cloud_links_service.addCloudLinkTable({tableId: this.tableName, settings_obj: this.table_settings_object, data: []}).subscribe(data=>{
-      if(!data.success){
-        this.flashmessage.show(JSON.stringify(data.msg), { cssClass: 'alert-danger', timeout: 3000 });
-      }else{
-        this.flashmessage.show('Table ' + JSON.stringify(data.cloudLinkTable.tableId) + ' Was Created ' , { cssClass: 'alert-success', timeout: 3000 });
+    this.cloud_links_service.addCloudLinkTable({
+      tableId: this.tableName,
+      settings_obj: this.table_settings_object,
+      data: []
+    }).subscribe(data => {
+      if (!data.success) {
+        this.flashmessage.show(JSON.stringify(data.msg), {cssClass: 'alert-danger', timeout: 3000});
+      } else {
+        this.flashmessage.show('Table ' + JSON.stringify(data.cloudLinkTable.tableId) + ' Was Created ', {
+          cssClass: 'alert-success',
+          timeout: 3000
+        });
       }
     });
   }
 
-  onCreateConfirm(event,tableId){
+  onCreateConfirm(event, tableId) {
     if (window.confirm('Are you sure you want to create?')) {
-      if(!(event.newData['url'].includes("http://")) && !(event.newData['url'].includes("https://"))){
+      if (!(event.newData['url'].includes("http://")) && !(event.newData['url'].includes("https://"))) {
         event.newData['url'] = "https://" + event.newData['url'];
       }
-      event.newData['url'] = '<a href="' + event.newData['url'] +'">' + event.newData['url'].replace("http://","") + '</a>';
+      event.newData['url'] = '<a href="' + event.newData['url'] + '">' + event.newData['url'].replace("http://", "") + '</a>';
       console.log(event.newData);
-      this.cloud_links_service.addEntryToCloudLinkTable(tableId.tableId,event.newData).subscribe(data=>{
-        if(!data.err){
-          this.flashmessage.show('new Entry was added to the table ' + tableId.tableId,{ cssClass: 'alert-success', timeout: 3000 });
-        }else{
-          this.flashmessage.show('Something Went Wrong',{ cssClass: 'alert-danger', timeout: 3000 })
+      this.cloud_links_service.addEntryToCloudLinkTable(tableId.tableId, event.newData).subscribe(data => {
+        if (!data.err) {
+          this.flashmessage.show('new Entry was added to the table ' + tableId.tableId, {
+            cssClass: 'alert-success',
+            timeout: 3000
+          });
+        } else {
+          this.flashmessage.show('Something Went Wrong', {cssClass: 'alert-danger', timeout: 3000})
         }
       });
       event.confirm.resolve(event.newData);
@@ -99,22 +109,40 @@ export class CloudlinksComponent implements OnInit {
     }
   }
 
-  onEditConfirm(event,tableId) {
+  onEditConfirm(event, tableId) {
     if (window.confirm('Are you sure you want to save?')) {
-      event.newData['name'] += ' + added in code';
+      this.cloud_links_service.deleteEntryFromCloudTable(tableId.tableId, event.data).subscribe(data => {
+        if (!data.success) {
+          this.flashmessage.show('Something Went Wrong on update delete', {cssClass: 'alert-danger', timeout: 3000})
+        } else {
+          this.cloud_links_service.addEntryToCloudLinkTable(tableId.tableId, event.newData).subscribe(data => {
+            if (!data.success) {
+              this.flashmessage.show('Something Went Wrong on update add', {cssClass: 'alert-danger', timeout: 3000})
+            } else {
+              this.flashmessage.show('An Entry was updated in the table ' + tableId.tableId, {
+                cssClass: 'alert-success',
+                timeout: 3000
+              })
+            }
+          })
+        }
+      });
       event.confirm.resolve(event.newData);
     } else {
       event.confirm.reject();
     }
   }
 
-  onDeleteConfirm(event,tableId){
+  onDeleteConfirm(event, tableId) {
     if (window.confirm('Are you sure you want to delete?')) {
-      this.cloud_links_service.deleteEntryFromCloudTable(tableId.tableId,event.data).subscribe(data=>{
-        if(!data.err){
-          this.flashmessage.show('A row was successfully deleted from the table ' + tableId.tableId,{ cssClass: 'alert-success', timeout: 3000 })
-        }else{
-          this.flashmessage.show('Something Went Wrong',{ cssClass: 'alert-danger', timeout: 3000 })
+      this.cloud_links_service.deleteEntryFromCloudTable(tableId.tableId, event.data).subscribe(data => {
+        if (!data.err) {
+          this.flashmessage.show('A row was successfully deleted from the table ' + tableId.tableId, {
+            cssClass: 'alert-success',
+            timeout: 3000
+          })
+        } else {
+          this.flashmessage.show('Something Went Wrong', {cssClass: 'alert-danger', timeout: 3000})
         }
       });
       event.confirm.resolve();
@@ -123,15 +151,18 @@ export class CloudlinksComponent implements OnInit {
     }
   }
 
-  deleteTable(){
-    this.tables_array.forEach((table,index) => {
-      if(table['tableId'] === this.tableName){
-        this.tables_array.splice(index,1);
-        this.cloud_links_service.deleteCloudLinkTable(this.tableName).subscribe(table=>{
-          if(!table.success){
-            this.flashmessage.show(table.msg,{ cssClass: 'alert-danger', timeout: 3000 })
-          }else{
-            this.flashmessage.show('Table ' + this.tableName + ' was deleted',{ cssClass: 'alert-success', timeout: 3000 })
+  deleteTable() {
+    this.tables_array.forEach((table, index) => {
+      if (table['tableId'] === this.tableName) {
+        this.tables_array.splice(index, 1);
+        this.cloud_links_service.deleteCloudLinkTable(this.tableName).subscribe(table => {
+          if (!table.success) {
+            this.flashmessage.show(table.msg, {cssClass: 'alert-danger', timeout: 3000})
+          } else {
+            this.flashmessage.show('Table ' + this.tableName + ' was deleted', {
+              cssClass: 'alert-success',
+              timeout: 3000
+            })
           }
         });
       }
