f5bfc421c1793ea671c72391c015d5d9ed788cee
Misha <mishaovodenko@gmail.com>
2018-03-01 21:47:06 +0200

    fixed timer on end

--- angular-src/src/app/vanhillequiz/questions/questions.component.ts
+++ angular-src/src/app/vanhillequiz/questions/questions.component.ts
@@ -21,10 +21,10 @@ export class QuestionsComponent implements OnInit {
   radiogroup: any;
   counterConfig: Object;
 
-  //calc User variables
+  // calc User variables
   CorrectAnswersForQuestions: any[] = [];
   AnswersOfUser: Number[] = [];
-  CorrectAnsPerDiff: any[] = [0, 0, 0, 0, 0];
+  // CorrectAnsPerDiff: [];
 
 
   constructor(private questionService: QuestionsserviceService,
@@ -198,12 +198,10 @@ export class QuestionsComponent implements OnInit {
   }
 
 
-  saveCorrectUserAnswers() {
-    var arrstring = "";
-    for (var index = 0; index < this.CorrectAnsPerDiff.length; index++) {
-      arrstring = arrstring + this.CorrectAnsPerDiff[index].toString() + '/';
-      console.log("arrstring: " + arrstring)
-
+  saveCorrectUserAnswers(resultArr: any[]) {
+    let arrstring = '';
+    for (let index = 0; index < resultArr.length; index++) {
+      arrstring = arrstring + resultArr[index].toString() + '/';
     }
     this.questionService.saveCorrectAnsPerDiff(this.User, arrstring, this.tryTime).subscribe(data => {
       if (data.success) {
@@ -214,89 +212,23 @@ export class QuestionsComponent implements OnInit {
     });
   }
 
-  //calcs the Users Correct Answers for difficulties
+  // calcs the Users Correct Answers for difficulties
   calcUser(finishedInTime) {
-    //handles last question
+    // handles last question
     if (finishedInTime) {
       this.saveAnswer();
+    } else {
+      this.showFinish = true;
     }
-    console.log("student calc started for student: " + this.User);
+    // console.log("student calc started for student: " + this.User);
     this.questionService.calcUser(this.User, this.tryTime).subscribe(userResult => {
       if (!userResult.success) {
 
       } else {
-        this.CorrectAnsPerDiff = userResult.studentRes;
-        this.saveCorrectUserAnswers();
+        // this.CorrectAnsPerDiff = userResult.studentRes;
+        this.saveCorrectUserAnswers(userResult.studentRes);
       }
     });
 
   }
-
-  //previous calc method - client side
-  // calcUser() {
-  //   this.saveAnswer();
-  //   console.log("calc start: " + this.CorrectAnsPerDiff);
-  //   this.questionService.getAllQuestions().subscribe(data => {
-  //     this.spinner.hide();
-  //     if (data.success) {
-  //       for (var index = 0; index < data.questions.length; index++) {
-  //         this.CorrectAnswersForQuestions[index] = {
-  //           corA: data.questions[index].correctA,
-  //           diff: data.questions[index].dif
-  //         };
-  //       }
-  //       this.questionService.getUser(this.User).subscribe(data => {
-  //         if (data.success) {
-  //           if (this.tryTime == 1) {
-  //             this.AnswersOfUser = data.user[0].Answers1;
-  //             if (this.Answers.length < 25) {
-  //               this.CorrectAnsPerDiff = [0, 0, 0, 0, 0];
-  //             }
-  //           }
-  //           if (this.tryTime == 2) {
-  //             this.AnswersOfUser = data.user[0].Answers2;
-  //             if (this.Answers.length < 25) {
-  //               this.CorrectAnsPerDiff = [0, 0, 0, 0, 0];
-  //             }
-  //           }
-  //           if (this.CorrectAnswersForQuestions.length == 0 || this.AnswersOfUser.length == 0) {
-  //             this.flashmessage.show('תשובות נכונות לשאלות או תשובות של סטודנט לא קיימים', {
-  //               cssClass: 'alert-danger',
-  //               timeout: 3000
-  //             });
-  //             return false;
-  //           } else {
-  //             for (var index = 0; index < this.CorrectAnswersForQuestions.length; index++) {
-  //               if (this.AnswersOfUser[index] == this.CorrectAnswersForQuestions[index].corA) {
-  //                 switch (parseInt(this.CorrectAnswersForQuestions[index].diff)) {
-  //                   case 1:
-  //                     this.CorrectAnsPerDiff[0] = parseInt(this.CorrectAnsPerDiff[0]) + 1;
-  //                     break;
-  //                   case 2:
-  //                     this.CorrectAnsPerDiff[1] = parseInt(this.CorrectAnsPerDiff[1]) + 1;
-  //                     break;
-  //                   case 3:
-  //                     this.CorrectAnsPerDiff[2] = parseInt(this.CorrectAnsPerDiff[2]) + 1;
-  //                     break;
-  //                   case 4:
-  //                     this.CorrectAnsPerDiff[3] = parseInt(this.CorrectAnsPerDiff[3]) + 1;
-  //                     break;
-  //                   case 5:
-  //                     this.CorrectAnsPerDiff[4] = parseInt(this.CorrectAnsPerDiff[4]) + 1;
-  //                     break;
-  //                 }
-  //               }
-  //             }
-  //             this.saveCorrectUserAnswers();
-  //
-  //           }
-  //         } else {
-  //           this.flashmessage.show('לא ניתן למצוא תשובות של סטודנט', {cssClass: 'alert-danger', timeout: 3000});
-  //         }
-  //       })
-  //     } else {
-  //       this.flashmessage.show('לא ניתן למצוא תשובות נכונות לשאלות', {cssClass: 'alert-danger', timeout: 3000});
-  //     }
-  //   });
-  // }
 }
