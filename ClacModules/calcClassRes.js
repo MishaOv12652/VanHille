52a1e8a048750950394dbb1a0025a7406a4284bc
Misha <MishaOvodenko@gmail.com>
2020-10-21 20:30:18 +0300

    fix calc class

--- ClacModules/calcClassRes.js
+++ ClacModules/calcClassRes.js
@@ -9,22 +9,22 @@ const studentModel = require('../models/VHStudent');
 const lodash = require('lodash/array');
 
 module.exports.calcClass = function (tryNum, courseNum, groupNum, callback) {
-    studentModel.getStudentsByCourseAndGroupNum(courseNum,groupNum, (err, students) => {
+    studentModel.getStudentsByCourseAndGroupNum(courseNum, groupNum, (err, students) => {
         if (err) {
-            return callback(err,[],-1,-1);
+            return callback(err, [], -1, -1);
         } else {
             if (students.length === 0) {
-                return callback("אין סטודנטים שעשו את השאלון עם מספרי הקורס או הקבוצות שבחרת",[],-1,-1);
+                return callback("אין סטודנטים שעשו את השאלון עם מספרי הקורס או הקבוצות שבחרת", [], -1, -1);
             } else {
                 numOfStudentsDoneTheQuiz = 0;
                 corAnswersPerDiffArray = [0, 0, 0, 0, 0];
                 students.forEach((student) => {
                     switch (parseFloat(tryNum)) {
                         case 1:
-                            arrayToAdd = student.correctAperdif1;
+                            arrayToAdd = student.correctAperdif1.length === 0 ? [0, 0, 0, 0, 0] : student.correctAperdif1;
                             break;
                         case 2:
-                            arrayToAdd = student.correctAperdif2;
+                            arrayToAdd = student.correctAperdif2.length === 0 ? [0, 0, 0, 0, 0] : student.correctAperdif2;
                             break;
                     }
                     corAnswersPerDiffArray = lodash.zipWith(corAnswersPerDiffArray, arrayToAdd, (a, b) => {
@@ -32,10 +32,10 @@ module.exports.calcClass = function (tryNum, courseNum, groupNum, callback) {
                     });
                     numOfStudentsDoneTheQuiz++;
                 });
-                for (let i=0;i<5;i++){
-                    corAnswersPerDiffArray[i] = (corAnswersPerDiffArray[i]/(numOfStudentsDoneTheQuiz*5))*100;
+                for (let i = 0; i < 5; i++) {
+                    corAnswersPerDiffArray[i] = (corAnswersPerDiffArray[i] / (numOfStudentsDoneTheQuiz * 5)) * 100;
                 }
-                return callback(null,corAnswersPerDiffArray,courseNum,groupNum);
+                return callback(null, corAnswersPerDiffArray, courseNum, groupNum);
             }
         }
     });
